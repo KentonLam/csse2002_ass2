23f0024edc24913fb0b1191a5ab59e25d6e4ecd7
Kenton Lam <kenton_lam@outlook.com>
2018-09-12 16:31:02 +1000

    Add testAddLinkedTilesNormal.

--- src/test/java/csse2002/block/world/SparseTileArrayTest.java
+++ src/test/java/csse2002/block/world/SparseTileArrayTest.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SparseTileArrayTest {
 
     SparseTileArray emptyArray;
@@ -27,7 +32,42 @@ public class SparseTileArrayTest {
     }
 
     @Test
-    public void testAddLinkedTilesNormal() {
+    public void testAddLinkedTilesNormal() throws BlockWorldException {
+        List<Tile> tiles = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            tiles.add(new Tile());
+        }
+        tiles.get(0).addExit("south", tiles.get(2));
+        tiles.get(2).addExit("west", tiles.get(1));
+        tiles.get(1).addExit("south", tiles.get(5));
+        tiles.get(5).addExit("east", tiles.get(7));
+        tiles.get(7).addExit("north", tiles.get(2));
+        tiles.get(2).addExit("south", tiles.get(7));
+        tiles.get(7).addExit("east", tiles.get(8));
+        tiles.get(8).addExit("north", tiles.get(3));
+        tiles.get(2).addExit("east", tiles.get(3));
+        tiles.get(3).addExit("east", tiles.get(4));
+        tiles.get(4).addExit("south", tiles.get(9));
+        tiles.get(9).addExit("west", tiles.get(8));
+
+        this.emptyArray.addLinkedTiles(tiles.get(0), 0, 0);
+
+        HashMap<Position, Tile> expectedMap = new HashMap<>();
+        expectedMap.put(new Position(0, 0), tiles.get(0));
+        expectedMap.put(new Position(-1, 1), tiles.get(1));
+        expectedMap.put(new Position(0, 1), tiles.get(2));
+        expectedMap.put(new Position(1, 1), tiles.get(3));
+        expectedMap.put(new Position(2, 1), tiles.get(4));
+        expectedMap.put(new Position(-1, 2), tiles.get(5));
+        expectedMap.put(new Position(0, 2), tiles.get(7));
+        expectedMap.put(new Position(1, 2), tiles.get(8));
+        expectedMap.put(new Position(2, 2), tiles.get(9));
+
+        for (Map.Entry<Position, Tile> entry : expectedMap.entrySet()) {
+            assertEquals("Incorrect tile inserted at position.",
+                    entry.getValue(),
+                    this.emptyArray.getTile(entry.getKey()));
+        }
 
     }
 }
