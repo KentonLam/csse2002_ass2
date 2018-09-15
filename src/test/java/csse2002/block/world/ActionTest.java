407698a72b35075966ca1ed0eaa2f3bacf7d7970
Kenton Lam <kenton_lam@outlook.com>
2018-09-16 08:19:39 +1000

    Added test for load action working normally.

--- src/test/java/csse2002/block/world/ActionTest.java
+++ src/test/java/csse2002/block/world/ActionTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.experimental.categories.Category;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -37,6 +38,7 @@ public class ActionTest
     public void testConstructorDoesntThrow() {
         Map<Integer, String> actionInputs = new HashMap<>();
         actionInputs.put(Action.DIG, "northnortheast");
+        actionInputs.put(Action.DROP, "-100");
         actionInputs.put(-100, "                  ");
         actionInputs.put(Integer.MAX_VALUE, null);
         actionInputs.put(Integer.MIN_VALUE, "west");
@@ -74,6 +76,11 @@ public class ActionTest
     }
 
     @Test(expected = ActionFormatException.class)
+    public void testLoadActionDigWithSpace() throws ActionFormatException {
+        Action.loadAction(makeReader("DIG "));
+    }
+
+    @Test(expected = ActionFormatException.class)
     public void testLoadActionMoveNoSecondary() throws ActionFormatException {
         Action.loadAction(makeReader("MOVE_BUILDER"));
     }
@@ -98,5 +105,35 @@ public class ActionTest
         }
         assertNull("Loading from EOF should be null.",
                 Action.loadAction(reader));
+    }
+
+    @Test
+    public void testLoadActionNormal() throws ActionFormatException {
+        // Overwrite instance fields with actions loaded from strings.
+        digAction = Action.loadAction(makeReader("DIG"));
+        moveBuilderAction = Action.loadAction(makeReader("MOVE_BUILDER east"));
+        moveBlockAction = Action.loadAction(makeReader("MOVE_BLOCK south"));
+        dropAction = Action.loadAction(makeReader("DROP 3"));
+
+        // None of these should throw.
+        assertEquals("Dig action primary wrong.",
+                Action.DIG, digAction.getPrimaryAction());
+        assertEquals("Dig action secondary wrong.",
+                "", digAction.getSecondaryAction());
+
+        assertEquals("Move builder primary wrong.",
+                Action.MOVE_BUILDER, moveBuilderAction.getPrimaryAction());
+        assertEquals("Move builder secondary wrong.",
+                "east", moveBuilderAction.getSecondaryAction());
+
+        assertEquals("Move block primary wrong.",
+                Action.MOVE_BLOCK, moveBlockAction.getPrimaryAction());
+        assertEquals("Move block secondary wrong.",
+                "south", moveBlockAction.getSecondaryAction());
+
+        assertEquals("Drop primary wrong.",
+                Action.DROP, dropAction.getPrimaryAction());
+        assertEquals("Drop secondary wrong.",
+                "3", dropAction.getSecondaryAction());
     }
 }
