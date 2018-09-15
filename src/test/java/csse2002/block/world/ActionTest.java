55b326eb445fc8be6110a5a2fa9d1e8e08a99f23
Kenton Lam <kenton_lam@outlook.com>
2018-09-16 09:01:43 +1000

    Remove unused import.

--- src/test/java/csse2002/block/world/ActionTest.java
+++ src/test/java/csse2002/block/world/ActionTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.Before;
-import org.junit.experimental.categories.Category;
 
 import java.io.BufferedReader;
 import java.io.IOException;
